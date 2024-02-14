9fcefced5bdc6520cde322682259c7332176f3e3
9ziggy9 <davidarogers@protonmail.com>
2024-02-14 01:34:29 -0800

    IO monad implementation, issue with chaining

--- src/main.ts
+++ src/main.ts
@@ -20,6 +20,53 @@ const Nothing = <T>(): Maybe<T> => ({
   getData:   () => null,
 });
 
+// Classic definition, but this can be augmented for any desired Nothing();
 const Maybe = <T>(data: T | null | undefined): Maybe<T> => data == null
   ? Nothing()
   : Just(data);
+
+let DANGEROUS_VALUE: {response?: string};
+setInterval(() => {
+  DANGEROUS_VALUE = Math.random() < 0.3 ? {} : {response: "We happy."};
+}, 1000);
+
+setInterval(() => {
+  // Note compiler whines that DANGEROUS_VALUE may not have response!
+  // Further note that try/catch will NOT work without certain compiler
+  // flags enabled. See `useUnknownInCatchVaribles`.
+
+  // Here is one classic approach. In a sense this is a null/undefined check.
+  // if ("response" in DANGEROUS_VALUE)
+  //   console.log("FOUND VALUE:", DANGEROUS_VALUE.response);
+
+  // USING Maybe monad
+  Maybe(DANGEROUS_VALUE.response)
+    .chain(response => Just(console.log("FOUND VALUE:", response)));
+
+}, 4000);
+
+// However, it is unfortunate that we cannot add isNothing() to chain,
+// as console.log() will return void value. To add this particular
+// side-effect, let's implement IO monad.
+
+type IO<T> = Monad<T> & {
+  run: () => T; // where the magical side-effects will happen.
+};
+
+const IO = <T>(effect: () => T): IO<T> => {
+  return {
+    run: effect,
+    chain: <U>(fn: (value: T) => IO<U>): IO<U> =>
+      IO(() => fn(effect()).run())
+  };
+};
+
+const log = <T>(msg: T): IO<T> => IO(() => {
+  console.log(msg);
+  return msg;
+});
+
+IO(() => 5)
+  .chain(n => IO(() => n + 1))
+  .chain(n => log(`Value: ${n}`))
+  .run();
