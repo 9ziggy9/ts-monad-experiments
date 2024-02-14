2c6d3eee073d845c59e6f1dacd2e385131248c72
9ziggy9 <davidarogers@protonmail.com>
2024-02-13 20:18:55 -0800

    endofunctor implementation

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,37 @@ type Frame = {
 
 type FrameCollection = {[id: Frame["id"]]: Frame};
 
-const xs: FrameCollection = {
-  "frame-1": {
-    id: "frame-1",
-    ref: {name: "hello", value: 10},
-  },
-  "frame-2": {
-    id: "frame-2",
-    ref: {name: "goodbye", value: 99},
-  },
+interface Endofunctor<T> {
+  map(fn: (element: T) => T): Endofunctor<T>;
+  log(): void;
 }
+
+const toFrames = (...fs: Frame[]): Endofunctor<Frame> => {
+  const __frames: FrameCollection = fs
+    .reduce<FrameCollection>((collection, frame) => ({
+      ...collection,
+      [frame.id]: frame,
+    }), {});
+  return {
+    map: (fn) => toFrames(...Object.values(
+      Object.entries(__frames)
+      .reduce<FrameCollection>((collection, [id, frame]) => ({
+        ...collection,
+        [id]: fn(frame)
+      }), {})
+    )),
+    log: () => console.log(JSON.stringify(__frames, null, 2))
+  };
+};
+
+const xs = toFrames(
+  {id: "frame-1", ref: {name: "hello", value: 10}},
+  {id: "frame-2", ref: {name: "goodbye", value: 99}},
+)
+
+xs.log();
+
+xs.map(frame => ({
+  ...frame,
+  ref: {name: frame.ref.name + "new", value: frame.ref.value * (-1)}
+})).log()
